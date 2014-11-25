a47863960bf31ddcaf4d859cfc56bb1ed8afaf71
Tomas Ruzicka <tomasruzicka@abdoc.net>
2014-11-25 03:55:26 +0100

    Transit app 0.1

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Source this script to populate working Bash context:
+# `source env.sh`
+
+
+export PEBBLE_PHONE="10.1.1.9"
--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,115 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
+// var normalizePrice = function(p) {
+//   return p.replace(' ', '').replace('Kč', 'Kc').replace('ě', 'e');
+// };
 
-main.show();
+var config = {
+  journeys: [
+    {name: 'Work > home', from: 'Namesti Miru', to: 'Marjanka', location: 'Praha'},
+    {name: 'Home > work', to: 'Namesti Miru', from: 'Marjanka', location: 'Praha'}
+  ]
+};
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
+var errorMessage = function(err) {
+  var card = new UI.Card({
+    title: 'Error'
+  });
+  card.body('Msg:' + error);
+  card.show();
+};
+
+var normalize = function(s) {
+  return s.replace('ě','e').replace('š','s').replace('č','c').replace('ř','r')
+  .replace('ž','z').replace('ý','y').replace('á','a').replace('é','e').replace('í','i');
+};
+
+var travelModeTag = function(steps) {
+  var tag = '';
+  for (var i = 0, j = steps.length; i < j; ++ i) {
+    tag += steps[i].travel_mode[0]; // get first letter for every step
+  }
+  return tag;
+};
+
+var mainScreen = function() {
+  var items = [];
+  for (var i = 0, j = config.journeys.length; i < j; ++ i) {
+    items.push({
+      title: config.journeys[i].name,
+      subtitle: config.journeys[i].from + ' > ' + config.journeys[i].to
+    });
+  }
+
+  var journeys = new UI.Menu({
     sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
+      title: 'Journeys',
+      items: items
     }]
   });
-  menu.on('select', function(e) {
+
+  journeys.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
+    transitScreen(config.journeys[e.itemIndex]);
   });
-  menu.show();
-});
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+  journeys.show();
+};
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+var transitScreen = function(journey) {
+  var from = encodeURIComponent(journey.from + ',' + journey.location);
+  var to = encodeURIComponent(journey.to + ',' + journey.location);
+  var url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+from+'&destination='+to+
+    '&mode=transit&departure_time=now&alternatives=true';
+  console.log(url);
+  ajax({ 'url': url, 'type': 'json' },
+    function(data) {
+      if (data.status != 'OK') return errorMessage(data.status);
+      var opts = [];
+      for (var i = 0, j = data.routes.length; i < j; ++ i) {
+        console.log('steps: '+data.routes[i].legs.length);
+        opts.push({
+          title: data.routes[i].legs[0].departure_time.text + ' > '+ travelModeTag(data.routes[i].legs[0].steps),
+          subtitle: normalize(data.routes[i].legs[0].steps[0].html_instructions)
+        });
+      }
+      var list = new UI.Menu({
+        sections: [{
+          title: 'Namesti Miru > Marjanka',
+          items: opts
+        }]
+      });
+      list.show();
+    },
+    function(error) {
+      errorMessage(error);
+    }
+  );
+};
+
+mainScreen();
+
+
+// main.on('click', 'select', function(e) {
+//   var wind = new UI.Window();
+//   var textfield = new UI.Text({
+//     position: new Vector2(0, 50),
+//     size: new Vector2(144, 30),
+//     font: 'gothic-24-bold',
+//     text: 'Text Anywhere!',
+//     textAlign: 'center'
+//   });
+//   wind.add(textfield);
+//   wind.show();
+// });
+//
+// main.on('click', 'down', function(e) {
+//   var card = new UI.Card();
+//   card.title('A Card');
+//   card.subtitle('Is a Window');
+//   card.body('The simplest window type in Pebble.js.');
+//   card.show();
+// });
